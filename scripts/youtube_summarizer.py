43191ecc0999ebaddbf334260b1e8346943933bf
canbinbinyouxiang <canbinbinyouxiang@gmail.com>
2026-06-29 17:57:50 +0800

    Update youtube_summarizer.py

--- scripts/youtube_summarizer.py
+++ scripts/youtube_summarizer.py
@@ -27,6 +27,8 @@ from youtube_transcript_api import (
 CHANNELS = [
     {"name": "oldpowerful",    "id": "UC8gZZWIWmBuCb_gzC8DUrvw"},
     {"name": "laomanpindao2049", "id": "UCrAC23izk57G7jCBPfdXGkg"},
+    {"name": "Asianometry", "id": "UC1LpsuAUaKoMzzJSEt5WImw"},
+    {"name": "Inside_China_Business", "id": "UCNlAaPtfHizB_k6wztaHmZg"},
 ]
 
 FEISHU_WEBHOOK_URL = os.environ["HORIZON_WEBHOOK_URL"]
